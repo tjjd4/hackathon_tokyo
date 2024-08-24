e6a9be699f4c50c1ed5dc602523d0f75982aaf82
tjjd4 <t01123581321345589@gmail.com>
2024-08-25 06:20:01 +0900

    feat: add component

--- src/components/SelfHeirAccount/Request.tsx
+++ src/components/SelfHeirAccount/Request.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { 
   BaseError,
-  useBalance,
   useReadContract,
-  useWatchContractEvent,
+  useWriteContract,
+  useWaitForTransactionReceipt,
 } from 'wagmi';
 import { Address, isAddress } from 'viem';
+
+import { contractAccountAbi } from '@/services/abi/contractAccountAbi';
 import { heirAccountAbi } from '@/services/abi/heirAccountAbi';
 
 interface RequestProps {
@@ -15,6 +17,17 @@ interface RequestProps {
 
 export const Request = ({ userAddress, selfHeirAccountAddress }: RequestProps) => {
   const [currentPredecessorAddress, setCurrentPredecessorAddress] = useState<Address | null>(null);
+
+  const { 
+    data: hash,
+    isPending,
+    error,
+    writeContract
+  } = useWriteContract();
+
+  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
+    hash,
+  });
 
   const { data: predecessorAddress, isLoading: isPredecessorAddressLoading, error: predecessorAddressError } = useReadContract({
     abi: heirAccountAbi,
@@ -34,5 +47,45 @@ export const Request = ({ userAddress, selfHeirAccountAddress }: RequestProps) =
     }
   }, [predecessorAddress, isPredecessorAddressLoading, predecessorAddressError])
 
-  
+  if (currentPredecessorAddress && isAddress(currentPredecessorAddress)) {
+    const {
+      data: config,
+      isLoading: isConfigLoading,
+      error: configError,
+      refetch: refetchConfig,
+    } = useReadContract({
+      abi: contractAccountAbi,
+      address: currentPredecessorAddress,
+      functionName: 'config',
+      args: [],
+    });
+  }
+
+  const handleRequest = async () => {
+    if (!currentPredecessorAddress) {
+      alert('No predecessor address found!');
+      return;
+    }
+
+    writeContract({
+      abi: contractAccountAbi,
+      address: currentPredecessorAddress,
+      functionName: 'requestInactiveAccount',
+      args: [], // Adjust the args based on the smart contract function signature
+    });
+  };
+
+  useEffect(() => {
+    if (isConfirmed) {
+      refetchConfig();
+    }
+    if (!isConfirming && !isConfirmed) {
+      console.log("Error: ", );
+    }
+  }, [isConfirming, isConfirmed])
+
+  return (
+    <>
+    </>
+  );
 }
